5119fcb09df35769f48336da60f16dbb31703d5d
sloth-april <sibata422@gmail.com>
2020-09-03 23:57:29 +0900

    最終画面

---
+++ html/js/searchGroup.js
@@ -0,0 +1,16 @@
+// ajaxで取得したデータをリストにして表示する
+function list_details(details){
+  $.each(details, function(index, value){
+    var list = $('<li class="list-group-item removal-li"></li>')
+    $(list).append('<label class="name" class="form-check-input" for="' + value[0] + '">' + value[1] + '</label>');
+    $(list).append('<input type="checkbox" id=' + value[0] + ' class="floatRight list-checkbox">');
+    $('.list-group').append(list);
+  });
+};
+
+function searchName(){
+   var search_name = $('#search').val();
+   alert(search_name);
+   // search_nameをサーバー側にajaxして検索処理をする
+   // 検索結果をlist_detail()を使用して再表示する
+};
