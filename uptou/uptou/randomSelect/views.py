87b44297e65eef660caa29839ce3e48e94bc3762
sloth-april <sibata422@gmail.com>
2020-12-09 23:12:05 +0900

    100%完成か!?

--- uptou/uptou/randomSelect/views.py
+++ uptou/uptou/randomSelect/views.py
@@ -23,15 +23,13 @@ def returnResult(request):
             radius = form.cleaned_data['radius']
             line = form.cleaned_data['line']
             location = getLocation(line, station)
+            print(station + ': ' + category + ': ' + str(radius))
             lat = location['x']
             lon = location['y']
             targetInfo = getPlaceInfo(category, radius, lat, lon)
-            print('---------------------')
-            print(targetInfo)
-            print('---------------------')
             if bool(targetInfo):
                 # ランダムな数字を取得し、情報を1つに絞る
-                content = demoSelectItem(targetInfo)
+                content = selectItem(targetInfo)
             else:
                 message = '利用可能なお店はありません'
                 content = {'message': message}
@@ -39,7 +37,8 @@ def returnResult(request):
             return render(request, 'randomSelect/selectedItem.html', content)
         else:
             form = getInfoForm
-            content = {'form': form}
+            message = '有効な値を選択してください'
+            content = {'form': form, 'message': message}
     else:
         form = getInfoForm
         message = '有効な情報を選択してください。'
@@ -48,27 +47,43 @@ def returnResult(request):
 
 # アイテムを取得し、利用可能なアイテムを返す
 def selectItem(targetInfo):
-    open = False
-    i = 1
-    while open == False:
-        count = len(targetInfo)
-        if(count != '' or count > 0):
-            inf = targetInfo[random.randrange(len(targetInfo))]
-            open = inf['opening_hours']['open_now']
-            if open == True:
-                name = inf['name']
-                url = inf['photos'][0]['html_attributions'][0]
-                level = inf['price_level']
-                content = {'name': name, 'open':open, 'url': url, 'level': level}
-                break
-            i+=1
-            if i > 15:
-                message = '周辺に利用可能なお店はありませんでした。'
-                content ={'message': message}
-                break
+    i = 0
+    content = {}
+    open = 'False'
+    count = len(targetInfo)
+    while i < count:
+        inf = targetInfo[random.randrange(0,count)]
+        print(inf)
+        if 'opening_hours' in inf.keys():
+            if 'open_now' in inf['opening_hours'].keys():
+                open = str(inf['opening_hours']['open_now'])
         else:
-            message = '利用可能なお店はありません。'
-            content = {'message': message}
+            open =  'notSure'
+        if open == 'True':
+            name = inf['name']
+            place_id = inf['place_id']
+            url = 'https://www.google.com/maps/place/?q=place_id:' + place_id
+            content = {'name': name, 'open':open, 'url': url}
+            if 'price_level' in inf.keys():
+                level = inf['price_level']
+                content['level'] = level
+            break
+        elif open == 'notSure':
+            name = inf['name']
+            place_id = inf['place_id']
+            url = 'https://www.google.com/maps/place/?q=place_id:' + place_id
+            content = {'name': name, 'open':open, 'url': url}
+            if 'price_level' in inf.keys():
+                level = inf['price_level']
+                content['level'] = level
+        else:
+            message = '周辺に利用可能なお店はありませんでした。'
+            content ={'message': message}
+        i += 1
+        print('------------------------')
+        print(open)
+        print('------------------------')
+        print(i)
     return content
 
 # 開発用メソッド
@@ -95,8 +110,10 @@ def demoSelectItem(targetInfo):
 def getPlaceInfo(category, radius, lat, lon):
     url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
     key = getKeyinfo()
+    category = []
+    category.append(category) 
     q = { 'location': str(lon) + ', ' + str(lat),
-          'type': category,
+          'types': category,
           'language': 'ja',
           'radius': radius,
           'key': key}
